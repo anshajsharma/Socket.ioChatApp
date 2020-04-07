b0e0ac8aef08bbc2bd2e8b71d6ac9c7342a6391e
ANSHAJ KUMAR <55790104+anshajsharma@users.noreply.github.com>
2020-04-08 01:05:59 +0530

    asd

---
+++ ReadMe.txt
@@ -0,0 +1,6 @@
+Level by level Comments:
+
+LEVEL 1:
+Socket.io installed and an instance of server is created using inbuild library 'http'
+
+Added connect and disconnect event listner...
--- server/server.js
+++ server/server.js
@@ -1,11 +1,29 @@
-const express = require('express');
+
 const path = require('path');
-var app = express();
+const http = require('http');
+const socketIO = require('socket.io');
 
+const express = require('express');
+let app = express();
 const PublicPath = path.join(__dirname,'/../Public')
-
 app.use(express.static( PublicPath ));
 
-app.listen(3000 , ()=>{
+let server = http.createServer(app);
+const port = process.env.PORT || 3000 ;
+
+let io = socketIO(server);   // that's why instance of server 'server' is created 
+
+io.on('connection',(socket)=>{
+    console.log('A new user is connected....');
+    socket.on('disconnect',()=>{
+        console.log('User was disconnected....');
+    })
+
+})
+
+
+
+
+server.listen(  port , ()=>{
     console.log('App is running at port 3000!!')
 })
